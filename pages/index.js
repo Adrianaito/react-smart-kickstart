b0064ff3db13d60c0e05ee73753723735f9f0d11
Adriana Ito <ito.dri@gmail.com>
2022-02-10 16:33:59 +0900

    add index page to show all campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import Card from "../components/Cards";
+import ButtonIcon from "../components/Button/ButtonIcon";
+import { PlusCircleIcon } from "@heroicons/react/solid";
 
-class CampaignIndex extends Component {
-  static async getInitialProps() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
-    return { campaigns };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1 className="text-red-500">{this.props.campaigns[0]}</h1>
+const CampaignIndex = ({ campaigns }) => {
+  const renderCards = () => {
+    return campaigns.map((address) => {
+      const items = {
+        address,
+        title: "Campaign Title",
+        body: "Campaign Body",
+        link: `/campaign/${address}`,
+      };
+      console.log(campaigns);
+      return <Card key={address} props={items} />;
+    });
+  };
+  return (
+    <>
+      <div className="customContainer px-4">
+        <h3>Open Campaigns</h3>
+        <div className="my-5">{renderCards()}</div>
+        <ButtonIcon
+          icon={<PlusCircleIcon className="h-5 w-5 pr-1 text-sky-400" />}
+          label="Start a Campaign"
+          onClick={() => {
+            console.log("button");
+          }}
+        />
       </div>
-    );
-  }
-}
+    </>
+  );
+};
+
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { campaigns };
+};
 
 export default CampaignIndex;
