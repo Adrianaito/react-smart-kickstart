c9e1313302185e822a2667a10e368ec9a79ff107
Adriana Ito <ito.dri@gmail.com>
2022-02-11 12:51:58 +0900

    add prop types

--- pages/campaign/[address].js
+++ pages/campaign/[address].js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import PropTypes from "prop-types";
 
 import web3 from "../../ethereum/web3";
 import campaignInstance from "../../ethereum/campaign";
@@ -8,6 +9,15 @@ import ShowCard from "../../components/ShowCard";
 import ContributeForm from "../../components/ContributeForm";
 import ButtonIcon from "../../components/Button/ButtonIcon";
 
+const propTypes = {
+  minimumContribution: PropTypes.number.isRequired,
+  balance: PropTypes.number.isRequired,
+  requestsCount: PropTypes.number.isRequired,
+  approversCount: PropTypes.number.isRequired,
+  manager: PropTypes.string.isRequired,
+  address: PropTypes.string.isRequired,
+};
+
 const Campaign = ({
   minimumContribution,
   balance,
@@ -15,51 +25,48 @@ const Campaign = ({
   approversCount,
   manager,
   address,
-}) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-10 max-w-4/5">
-      <div className="customContainer mt-5 max-w-4/5 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10 col-span-3">
-        <ShowCard
-          title={manager}
-          meta="Manager Address"
-          description="The manager of this campaign"
-        />
-        <ShowCard
-          title={minimumContribution}
-          meta="Minimum Contribution"
-          description="This is the minimum contribution required to contribute to this campaign"
-        />
-        <ShowCard
-          title={requestsCount}
-          meta="Number of Requests"
-          description="This is the number of requests made to this campaign. Requests must be approve by at least 50% of the campaign approvers"
-        />
-        <ShowCard
-          title={approversCount}
-          meta="Number of Approvers"
-          description="This is the number of people who have contribute to this campaign"
-        />
-        <ShowCard
-          title={web3.utils.fromWei(balance, "ether")}
-          meta="Campaign Balance(ether)"
-          description="This is the balance left of this campaign"
-        />
-      </div>
-      <div>
-        <ContributeForm address={address} />
-        <Link href={`/campaign/${address}/requests`} passHref>
-          <ButtonIcon label="Requests" />
-        </Link>
-      </div>
+}) => (
+  <div className="grid grid-cols-1 md:grid-cols-4 gap-10 max-w-4/5">
+    <div className="customContainer mt-5 max-w-4/5 grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-10 col-span-3">
+      <ShowCard
+        title={manager}
+        meta="Manager Address"
+        description="The manager of this campaign"
+      />
+      <ShowCard
+        title={minimumContribution}
+        meta="Minimum Contribution"
+        description="This is the minimum contribution required to contribute to this campaign"
+      />
+      <ShowCard
+        title={requestsCount}
+        meta="Number of Requests"
+        description="This is the number of requests made to this campaign. Requests must be approve by at least 50% of the campaign approvers"
+      />
+      <ShowCard
+        title={approversCount}
+        meta="Number of Approvers"
+        description="This is the number of people who have contribute to this campaign"
+      />
+      <ShowCard
+        title={web3.utils.fromWei(balance, "ether")}
+        meta="Campaign Balance(ether)"
+        description="This is the balance left of this campaign"
+      />
     </div>
-  );
-};
+    <div>
+      <ContributeForm address={address} />
+      <Link href={`/campaign/${address}/requests`} passHref>
+        <ButtonIcon label="Requests" />
+      </Link>
+    </div>
+  </div>
+);
 
 Campaign.getInitialProps = async (props) => {
-  const address = props.query.address;
+  const { address } = props.query;
   const campaign = await campaignInstance(props.query.address);
   const summary = await campaign.methods.getSummary().call();
-  console.log(summary[0]);
   return {
     minimumContribution: summary[0],
     balance: summary[1],
@@ -70,4 +77,5 @@ Campaign.getInitialProps = async (props) => {
   };
 };
 
+Campaign.propTypes = propTypes;
 export default Campaign;
