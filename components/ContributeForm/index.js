73d60dfbd1168f67ba0745faf6a9b3e2e8e9e8b5
Adriana Ito <ito.dri@gmail.com>
2022-02-11 11:41:34 +0900

    create form to contribute to a campaign

---
+++ components/ContributeForm/index.js
@@ -0,0 +1,89 @@
+import React, { useRef } from "react";
+import { Form, Formik, Field } from "formik";
+import * as Yup from "yup";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+
+import { update, success } from "../../components/Toast";
+import ButtonIcon from "../../components/Button/ButtonIcon";
+
+import campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+
+const ContributeForm = ({ address }) => {
+  const toastId = useRef(null);
+  const router = useRouter();
+
+  const validationSchema = Yup.object({
+    amount: Yup.number().required(),
+  });
+
+  const initialValues = {
+    amount: "",
+    address: address,
+  };
+
+  const handleSubmit = async ({ address, amount }) => {
+    toastId.current = toast("Waiting for approval...", { autoClose: false });
+    const campaignInstance = campaign(address);
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaignInstance.methods
+        .contribute()
+        .send({
+          from: accounts[0],
+          value: web3.utils.toWei(amount, "ether"),
+        })
+        .then(() =>
+          update(toastId.current, "Processing transaction...", "default", true)
+        );
+      update(toastId.current, "Contributed successfully!", "success", false);
+    } catch (err) {
+      update(toastId.current, err.message, "error", false);
+    }
+    router.reload();
+  };
+
+  return (
+    <div className="customContainer mb-5">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        validateOnMount
+        enableReinitialize
+        onSubmit={handleSubmit}
+      >
+        {({ values, isValid, setFieldValue, handleBlur }) => (
+          <>
+            {/* <pre>{JSON.stringify(values)}</pre> */}
+            <Form>
+              <div className="mb-6">
+                <label
+                  htmlFor="base-input"
+                  className="block my-2 ml-1  text-xl font-medium text-gray-900 dark:text-gray-900"
+                >
+                  Contribute
+                </label>
+                <div className="flex">
+                  <Field
+                    type="text"
+                    name="amount"
+                    onChange={(e) => setFieldValue("amount", e.target.value)}
+                    onBlur={handleBlur}
+                    className="lg:w-64 bg-gray-500 border border-gray-300 text-gray-600 lg:text-lg rounded-l-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  />
+                  <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-r-md border border-r-0 border-gray-300 dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                    ether
+                  </span>
+                </div>
+              </div>
+              <ButtonIcon label="Submit" type="submit" isValid={!isValid} />
+            </Form>
+          </>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default ContributeForm;
