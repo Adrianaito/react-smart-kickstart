912d6b00af7c07819d58984797533e179763d468
Adriana Ito <ito.dri@gmail.com>
2022-02-12 11:20:14 +0900

    adds prop types to button component

--- components/Button/ButtonIcon/index.js
+++ components/Button/ButtonIcon/index.js
@@ -1,22 +1,33 @@
 import React, { forwardRef } from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.element,
+  onClick: PropTypes.func,
+  isValid: PropTypes.bool,
+};
 
 const ButtonIcon = forwardRef(
-  ({ icon = null, onClick, label = null, isValid }, ref) => {
-    return (
-      <>
-        <button
-          type="submit"
-          className="button"
-          onClick={onClick}
-          disabled={isValid}
-          ref={ref}
-        >
-          {icon}
-          {label}
-        </button>
-      </>
-    );
-  }
+  ({ icon = null, onClick, label, isValid }, ref) => (
+    <button
+      type="submit"
+      className="button"
+      onClick={onClick}
+      disabled={isValid}
+      ref={ref}
+    >
+      {icon}
+      {label}
+    </button>
+  )
 );
 
+ButtonIcon.propTypes = propTypes;
+ButtonIcon.defaultProps = {
+  icon: null,
+  isValid: false,
+  onClick: null,
+  label: null,
+};
 export default ButtonIcon;
